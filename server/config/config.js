739764ae1c8f66de46b014de320e15e0400d7141
ItzaeAcosta <itzae.acosta@ojo7.com>
2019-11-27 16:34:38 -0600

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://userTest01:userTest01@cluster0-hjgne.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
